ed016128f984ca295fb71cabdbcdd775e34b01b0
Ellie Park <elliepark1012@gmail.com>
2023-01-18 19:22:36 -0500

    fixed the errors

--- thunder-server/app/controllers/application_controller.rb
+++ thunder-server/app/controllers/application_controller.rb
@@ -2,12 +2,10 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
-  end
 
   get "/events" do 
-    event = Events.all.order
-    event.to_json
+    events = Event.all
+    events.to_json
   end
+
 end
